5600f734e7726e692e25cacc86ae86cd0c77bb84
Keiran <keiran@incel.email>
2025-08-12 04:39:43 +0100

    default to system clipboard

--- lua/racc/init.lua
+++ lua/racc/init.lua
@@ -1,13 +1,17 @@
 local M = {}
 local commands = require("racc.commands")
 
-function M.setup()
+M.config = {
+	register = "+",
+}
+
+function M.setup(opts)
+	M.config = vim.tbl_extend("force", M.config, opts or {})
+	commands.setup(M.config)
 	vim.api.nvim_create_user_command("Racc", commands.check_status, { desc = "Check Raccoon API status" })
-	vim.api.nvim_create_user_command(
-		"RaccPlease",
-		commands.get_raccoon_url,
-		{ desc = "Get raccoon image URL and copy to register" }
-	)
+	vim.api.nvim_create_user_command("RaccPlease", function()
+		commands.get_raccoon_url(M.config.register)
+	end, { desc = "Get raccoon image URL and copy to register" })
 end
 
 return M
